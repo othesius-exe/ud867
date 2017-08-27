4acfec631f1874dd28bb8bd5b5c3a032aec1de71
othesius-exe <othesius.exe@gmail.com>
2017-08-27 16:51:53 -0400

    Fixed Tests

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/androidTest/EndpointsAsyncTaskTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/androidTest/EndpointsAsyncTaskTest.java
@@ -1,14 +1,11 @@
 package com.udacity.gradle.androidTest;
 
-import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
 
-import com.example.androidjokelibrary.JokeDisplayIntent;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -21,19 +18,14 @@ import java.util.concurrent.TimeUnit;
 @RunWith(JUnit4.class)
 public class EndpointsAsyncTaskTest {
 
-    @Before
-    public void checkThatActivityIsStarted() throws Exception {
-        Instrumentation.ActivityMonitor am = InstrumentationRegistry.getInstrumentation().addMonitor(JokeDisplayIntent.class.getName(), null, true);
-        am.waitForActivityWithTimeout(10);
-        //Assert.assertEquals(1, am.getHits());
-    }
-
     @Test
     public void testJokeIsNotEmptyString() throws Exception {
-        EndpointsAsyncTask getJokeTask = new EndpointsAsyncTask(InstrumentationRegistry.getContext());
+        EndpointsAsyncTask getJokeTask = new EndpointsAsyncTask(InstrumentationRegistry.getTargetContext());
         getJokeTask.execute();
         String joke = getJokeTask.get(30, TimeUnit.SECONDS);
         Assert.assertNotNull(joke);
+        Assert.assertTrue(!joke.isEmpty());
+        System.out.println(joke);
     }
 
 }
